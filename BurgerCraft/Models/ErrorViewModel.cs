5099dbad12efe905bf12dc895b335c3cd0444011
mb30942 <mb30942@seeu.edu.mk>
2025-01-11 10:53:54 +0100

    initial push

---
+++ BurgerCraft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BurgerCraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
